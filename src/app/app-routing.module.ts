3d12e1e424e7a7bdc2331955f752e24c1c54cc86
Raúl Salazar <salazar.roldan.97@gmail.com>
2021-02-03 21:36:08 +0100

    TfcDia03

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './componentes/home/home.component';
+import { NavegacionComponent } from './componentes/navegacion/navegacion.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "", component:HomeComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
